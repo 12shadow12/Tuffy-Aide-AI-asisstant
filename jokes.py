7687f240d2a4dfcafbd1c9978d1959d10a1f4f46
Donald Novasky <dnovasky@hotmail.com>
2022-10-20 11:46:17 -0700

    Switched to a new API and finished the jokes module.

--- jokes.py
+++ jokes.py
@@ -2,11 +2,16 @@
 import requests
 
 
-url = 'https://api.jokes.one/jod?category=knock-knock'
-headers = {'content-type': 'application/json',}
+url = 'https://v2.jokeapi.dev/joke/'
+#headers = {'content-type': 'application/json',}
 
-response = requests.get(url, headers=headers)
-#print(response)
-#print(response.text)
-joke=response.json()['contents']['jokes'][0]
-print((joke["joke"]["text"]).strip())
+joke = []
+def getJoke(type='Any'):
+    response = requests.get(url + type).json()
+    if not response['error']:
+        if response['type'] == 'single':
+            joke.append('single')
+            joke.append(response['joke'])
+        elif response['type'] == 'twopart':
+            joke.append(response['setup'])
+            joke.append(response['delivery'])

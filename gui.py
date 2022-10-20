b942ed32e737f19e01bdcd941b63c7cb5e03318e
mrmarlrico <mrmarlrico@gmail.com>
2022-10-19 21:11:05 -0700

    Added gui.py

--- gui.py
+++ gui.py
@@ -1,13 +1,55 @@
 #!/usr/bin/env python3
 
 from tkinter import *
-top = Tk()
+from PIL import ImageTk, Image
 
-def helloCallBack():
-   label = Message( "Hello Python", "Hello World")
-   return label
+# Need to do:
+# Function to stop the interaction
+# Function to start the interaction
 
-B = Button(top, text ="Hello", command = helloCallBack)
+# Main window
+root = Tk()
+root.title("Tuffy Aide")
+root.geometry("1200x700")
+#Keeps the window from resizing
+root.resizable(0,0)
+icon = Image.open("icon.png")
+icon = ImageTk.PhotoImage(icon)
+root.iconphoto(False, icon)
+root.configure(background="#FF7900")
 
-B.pack()
-top.mainloop()
+# Title text
+Label(root, text="Tuffy Aide", bg="#FF7900", font=("Times 40 bold")).pack()
+
+
+# Tuffy character photo
+tuffy = Image.open("tuffy.png").resize((200,250))
+tuffy = ImageTk.PhotoImage(tuffy)
+Label(root, image=tuffy).pack()
+
+# Profile button
+prof = Image.open("prof.png").resize((50,50))
+prof = ImageTk.PhotoImage(prof)
+prof_button = Menubutton(root, image=prof, activebackground="black")
+prof_button.menu = Menu(prof_button, tearoff=0)
+prof_button["menu"] = prof_button.menu
+
+# Figure out how to implement a button to open a window and then create a profile through that window
+var1 = IntVar()
+
+  
+prof_button.menu.add_checkbutton(label = "Add profile", variable = var1)  
+prof_button.place(x=1020, y=5)
+
+# Microphone button
+mic = Image.open("mic.png").resize((50, 50))
+mic = ImageTk.PhotoImage(mic)
+Button(root, image=mic).place(x=1080, y=5)
+
+# Exit interaction buttom
+ext = Image.open("x.png").resize((50,50))
+ext = ImageTk.PhotoImage(ext)
+Button(root, image=ext).place(x=1140, y=5)
+
+
+root.mainloop()
--- main.py
+++ main.py
@@ -1,1 +1,63 @@
-print("Hello World!")
+import pyttsx3
+import speech_recognition as sr
+import weather
+import greetings
+
+
+def say(audio: str) -> None:
+    
+    tts = pyttsx3.init()
+    voices = tts.getProperty('voices')
+
+    tts.setProperty('voice', voices[1].id)
+    tts.setProperty('rate', 140)
+
+    tts.say(audio)
+    tts.runAndWait()
+
+
+def command() -> str:
+
+    recognizer = sr.Recognizer()
+    command = None
+    
+    with sr.Microphone() as source:
+
+        recognizer.pause_threshold = .6
+        audio = recognizer.listen(source, 5)
+
+        try:
+            command = recognizer.recognize_google(audio, language='en')
+            
+        except:
+            say("I'm sorry, I could not understand.")
+        
+        return command
+
+
+def greet():
+    say(greetings.greet)
+
+
+def listenForCommand():
+    #greet()
+    say("How can i help you?")
+
+    while(True):
+
+        c = command().lower()
+        print(c)
+        if "weather" in c:
+            city = c[-1]
+            print(c)
+            print(city)
+            # w = weather.Weather().getWeather(city)
+            # desc = w["desc"]
+            # temp = w["temp"]
+            # high = w["high"]
+            # low = w["low"]
+
+            # say(f'The current weather in {city} is {desc} and {temp} degrees with a high of {high} and a low of {low}.')
+
+
+listenForCommand()
